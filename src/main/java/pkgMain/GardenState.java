eb8177a92e58e2884366a86ae885fba0b7f3f157
joseph loporto <joelopor@udel.edu>
2021-04-05 18:35:55 -0400

    GardenState stub

---
+++ src/main/java/pkgMain/GardenState.java
@@ -0,0 +1,40 @@
+package pkgMain;
+
+public class GardenState {
+	String gardenName;
+	String lastSaved;
+	int lepsSupported;
+	boolean isFull;
+	GardenPentagon shape;
+	
+	
+	
+	
+	void updateLeps() {
+		
+	}
+	
+	void updateGarden() {
+		
+	}
+	
+	void updateBudget() {
+		
+	}
+	
+	void checkBoundary() {
+		
+	}
+	
+	void placePlant() {
+		
+	}
+	
+	void hoverPLant() {
+		
+	}
+	
+	void deletePlant() {
+		
+	}
+}
