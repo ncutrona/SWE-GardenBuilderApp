e91fd0fd536923189a33ea244691f700a68cd562
joseph loporto <joelopor@udel.edu>
2021-04-05 18:51:20 -0400

    Controller stubbing

--- src/main/java/pkgMain/Controller.java
+++ src/main/java/pkgMain/Controller.java
@@ -1,5 +1,47 @@
 package pkgMain;
 
 public class Controller {
-
+	Model model;
+	View view;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	void start(){
+		
+	}
+	
+	void saveGardenHandler() {
+		
+	}
+	
+	void switchPageHandler() {
+		
+	}
+	
+	void popUpHandler() {
+		
+	}
+	
+	void conditionScreenHandler() {
+		
+	}
+	
+	void budgetTextHandler() {
+		
+	}
+	
+	void loadGardenHandler() {
+		
+	}
+	
+	void hoverHandler() {
+		
+	}
+	
+	void dragDropHandler() {
+		
+	}
+	
 }
