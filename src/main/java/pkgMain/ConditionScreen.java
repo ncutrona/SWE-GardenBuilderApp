bc4d08b019ad0d779b3dbcd14ba06985aac689a0
joseph loporto <joelopor@udel.edu>
2021-04-05 18:45:23 -0400

    ConditionScreen stub

---
+++ src/main/java/pkgMain/ConditionScreen.java
@@ -0,0 +1,18 @@
+package pkgMain;
+
+public class ConditionScreen {
+	Image moistureOne;
+	Image moistureTwo;
+	Image moistureThree;
+	Image soilOne;
+	Image soilTwo;
+	Image soilThree;
+	Image sunOne;
+	Image sunTwo;
+	Image sunThree;
+	
+	
+	void displayConditionsScreen() {
+		
+	}
+}
