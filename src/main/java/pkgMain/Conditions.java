0b8fdcb64f9bcd82dbd526dec9566fe14474b034
Khaleeb <caldavis@udel.edu>
2021-04-05 18:59:31 -0400

    add: setup enums in conditions

--- src/main/java/pkgMain/Conditions.java
+++ src/main/java/pkgMain/Conditions.java
@@ -1,5 +1,22 @@
 package pkgMain;
 
-public enum Conditions {
+public class Conditions {
+	
+	public enum sunCondition {
+		LESS, NORMAL, MORE;
+	}
+	
+	public enum moistureCondition {
+		LESS, NORMAL, MORE;
+	}
+	
+	public enum soilCondition {
+		// not sure?
+		WORSE, NORMAL, BETTER;
+	}
+	
+	
+	
+	
 
 }
