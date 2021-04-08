4ed936c28655ffd27235b34d80d67ee665eeed44
tommycheung24 <tcheung@udel.edu>
2021-04-07 20:42:13 -0400

    Add coordinates.java and added atribute to pentagonshape.java

---
+++ src/main/java/pkgMain/Coodinates.java
@@ -0,0 +1,17 @@
+package pkgMain;
+
+class Coordinates{
+	double x, y;
+	
+	public Coordinates(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	public double getY() {
+		return this.y;
+	}
+}
--- src/main/java/pkgMain/PentagonShape.java
+++ src/main/java/pkgMain/PentagonShape.java
@@ -1,5 +1,35 @@
 package pkgMain;
 
 public class PentagonShape {
+	
+	Coordinates top;
+	Coordinates left;
+	Coordinates leftdown;
+	Coordinates right;
+	Coordinates rightdown;
+	
+	public PentagonShape(int height, int width) {
+		top = new Coordinates(0,0);
+		left = new Coordinates(0,0);
+		leftdown = new Coordinates(0,0);
+		right = new Coordinates(0,0);
+		rightdown = new Coordinates(0,0);
+	}
+	
+	public Coordinates getTop() {
+		return this.top;
+	}
+	public Coordinates getLeft() {
+		return this.left;
+	}
+	public Coordinates getLeftdown() {
+		return this.leftdown;
+	}
+	public Coordinates getRight() {
+		return this.right;
+	}
+	public Coordinates getRightdown() {
+		return this.rightdown;
+	}
 
 }
