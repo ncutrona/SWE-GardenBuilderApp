36c63b33b94a8c010114b6b1b7f9b21249596bb7
Nicolas Cutrona <nicolascutrona@gmail.com>
2021-04-11 17:47:50 -0400

    Added the garden state functionality of setting hte bugdget to what the user has inputted.

--- src/main/java/pkgMain/CodeBreakDown4.java
+++ src/main/java/pkgMain/CodeBreakDown4.java
@@ -24,6 +24,8 @@ import javafx.stage.Stage;
 
 public class CodeBreakDown4 extends Application {
 	
+	GardenState garden = new GardenState("garden",  "today", 0, false, 0 /*PentagonShape gardenShape*/);
+	
 	public void start(Stage stage) {
 		
 		stage.setTitle("Garden Builder v. 0.01 (Alpha)");
@@ -50,21 +52,7 @@ public class CodeBreakDown4 extends Application {
 		slider.setBlockIncrement(1);
 		slider.setSnapToTicks(true);
 		
-
-		Slider slider2 = new Slider(1, 3, 2);
-		slider2.setMin(1);
-		slider2.setMax(3);
-		slider2.setValue(2);
-		slider2.setMajorTickUnit(3);
-		slider2.setMinorTickCount(2);
-		slider2.setShowTickLabels(true);
-		slider2.setShowTickMarks(true);
-		slider2.setBlockIncrement(1);
-		slider2.setSnapToTicks(true);
 		
-		TilePane tile = new TilePane(slider);
-		tile.getChildren().add(slider2);
-
 		BorderPane border = new BorderPane(slider);
 		border.setTop(box);
 		
@@ -75,6 +63,10 @@ public class CodeBreakDown4 extends Application {
 			        	try {
 			        		int intBudget = Integer.parseInt(budget.getText());
 			        		budget.setText("Your budget was set.");
+			        		
+			        		//Garden State Budget is now updated based off of the user input.
+			        		garden.setGardenBudget(intBudget);
+			        		System.out.println(garden.getGardenBudget());
 			        	} catch(Exception except) {
 			        		budget.setText("");
 			        		budget.setPromptText("Enter a valid budget $");
@@ -87,7 +79,8 @@ public class CodeBreakDown4 extends Application {
 			 });
 
 
-    	Scene scene = new Scene(tile, 800, 600);
+		
+    	Scene scene = new Scene(border, 800, 600);
         stage.setScene(scene);
         stage.show();
    	
