3ef98368ad797d8992d62a542637d2e3079fed48
Nicolas Cutrona <nicolascutrona@gmail.com>
2021-04-11 17:35:47 -0400

    NA

--- src/main/java/pkgMain/CodeBreakDown4.java
+++ src/main/java/pkgMain/CodeBreakDown4.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 public class CodeBreakDown4 extends Application {
@@ -27,9 +28,21 @@ public class CodeBreakDown4 extends Application {
 		slider.setBlockIncrement(1);
 		slider.setSnapToTicks(true);
 		
-		BorderPane border = new BorderPane(slider);
+		Slider slider2 = new Slider(1, 3, 2);
+		slider2.setMin(1);
+		slider2.setMax(3);
+		slider2.setValue(2);
+		slider2.setMajorTickUnit(3);
+		slider2.setMinorTickCount(2);
+		slider2.setShowTickLabels(true);
+		slider2.setShowTickMarks(true);
+		slider2.setBlockIncrement(1);
+		slider2.setSnapToTicks(true);
+		
+		TilePane tile = new TilePane(slider);
+		tile.getChildren().add(slider2);
 
-    	Scene scene = new Scene(border, 800, 600);
+    	Scene scene = new Scene(tile, 800, 600);
         stage.setScene(scene);
         stage.show();
 		
