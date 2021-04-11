7742a6a3a0511cb1cd617bc45573a238e8cc1d2c
Nicolas Cutrona <nicolascutrona@gmail.com>
2021-04-11 19:50:04 -0400

    Added the Clear Button and enabled a user to submit a garden name

--- src/main/java/pkgMain/CodeBreakDown4.java
+++ src/main/java/pkgMain/CodeBreakDown4.java
@@ -33,14 +33,20 @@ public class CodeBreakDown4 extends Application {
 		TextField budget = new TextField();
 		budget.setPromptText("Enter your Budget $");
 		Label budgetLabel = new Label("Budget $: ");
+		TextField gardenName = new TextField();
+		gardenName.setPromptText("Enter Your Garden Name: ");
+		Label gardenLabel = new Label("Garden Name: ");
+		
 		
 		Button submit = new Button("Submit");
 		Button clear = new Button("Clear");
 		
 		HBox box = new HBox(5);
 		box.setPadding(new Insets(5, 5, 5, 5));
-		box.getChildren().addAll(budgetLabel, budget, submit, clear);
+		box.getChildren().addAll(budgetLabel, budget, gardenLabel, gardenName, submit, clear);
 		
+		
+		//SLIDERS
 		Slider slider = new Slider(1, 3, 2);
 		slider.setMin(1);
 		slider.setMax(3);
@@ -52,31 +58,71 @@ public class CodeBreakDown4 extends Application {
 		slider.setBlockIncrement(1);
 		slider.setSnapToTicks(true);
 		
+		Slider slider2 = new Slider(1, 3, 2);
+		slider2.setMin(1);
+		slider2.setMax(3);
+		slider2.setValue(2);
+		slider2.setMajorTickUnit(3);
+		slider2.setMinorTickCount(2);
+		slider2.setShowTickLabels(true);
+		slider2.setShowTickMarks(true);
+		slider2.setBlockIncrement(1);
+		slider2.setSnapToTicks(true);
 		
-		BorderPane border = new BorderPane(slider);
+		Slider slider3 = new Slider(1, 3, 2);
+		slider3.setMin(1);
+		slider3.setMax(3);
+		slider3.setValue(2);
+		slider3.setMajorTickUnit(3);
+		slider3.setMinorTickCount(2);
+		slider3.setShowTickLabels(true);
+		slider3.setShowTickMarks(true);
+		slider3.setBlockIncrement(1);
+		slider3.setSnapToTicks(true);
+		
+		
+		BorderPane border = new BorderPane();
+		border.setLeft(slider);
+		border.setCenter(slider2);
+		border.setRight(slider3);
 		border.setTop(box);
 		
 		submit.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			    public void handle(ActionEvent e) {
-			        if ((budget.getText() != null && !budget.getText().isEmpty())) {
+			        if ((budget.getText() != null && !budget.getText().isEmpty() && gardenName.getText() != null && !gardenName.getText().isEmpty())) {
 			        	try {
 			        		int intBudget = Integer.parseInt(budget.getText());
 			        		budget.setText("Your budget was set.");
+			        		gardenName.setText("Your Garden Name was set.");
 			        		
 			        		//Garden State Budget is now updated based off of the user input.
 			        		garden.setGardenBudget(intBudget);
-			        		System.out.println(garden.getGardenBudget());
 			        	} catch(Exception except) {
 			        		budget.setText("");
 			        		budget.setPromptText("Enter a valid budget $");
+			        		gardenName.setText("");
+			        		gardenName.setPromptText("Enter Your Garden Name: ");
 			        	}
 			            
 			        } else {
 			            budget.setPromptText("Enter a budget $");
+			            gardenName.setPromptText("Enter a Garden Name: ");
 			        }
 			     }
 			 });
+		
+		//Setting an action for the Clear button
+		clear.setOnAction(new EventHandler<ActionEvent>() {
+
+		@Override
+		    public void handle(ActionEvent e) {
+				budget.clear();
+				budgetLabel.setText(null);
+				gardenName.clear();
+				gardenLabel.setText(null);
+		    }
+		});
 
 
 		
