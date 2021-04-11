1914441e93058dd50c53c8b6801f93affecdd849
Nicolas Cutrona <nicolascutrona@gmail.com>
2021-04-11 16:22:05 -0400

    Added Sldier example to the the conditions screen

--- src/main/java/pkgMain/CodeBreakDown4.java
+++ src/main/java/pkgMain/CodeBreakDown4.java
@@ -2,12 +2,39 @@ package pkgMain;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class CodeBreakDown4 extends Application {
 	
+	
+	
+	
+	public void start(Stage stage) {
+		
+		stage.setTitle("Garden Builder v. 0.01 (Alpha)");
+		
+		Slider slider = new Slider();
+		slider.setMin(0);
+		slider.setMax(100);
+		slider.setValue(40);
+		slider.setShowTickLabels(true);
+		slider.setShowTickMarks(true);
+		slider.setMajorTickUnit(50);
+		slider.setMinorTickCount(5);
+		slider.setBlockIncrement(10);
+		
+		
+		BorderPane border = new BorderPane(slider);
 
+    	Scene scene = new Scene(border, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+		
+	}
 }
