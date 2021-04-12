80cc0e2aa575d9b8025e0d467b45a2392a3b6f69
Khaleeb <caldavis@udel.edu>
2021-04-12 18:10:47 -0400

    add: Javadoc for Coordinates class

--- src/main/java/pkgMain/Coodinates.java
+++ src/main/java/pkgMain/Coodinates.java
@@ -1,16 +1,47 @@
 package pkgMain;
 
+
+/**
+ * Coordinates class, usable x,y coordinates for saving location
+ * 
+ * @author Aidan Chao
+ * @author Nicholas Cutrona
+ * @author Caleb Davis
+ * @author Joey Loporto
+ * @author Tommy Cheung
+ */
 class Coordinates{
 	double x, y;
 	
+	
+	/**
+	 * Constructor for Coordinates object,
+	 * Sets x and y from passed in doubles
+	 * 
+	 * @param x double x position
+	 * @param y double y position
+	 */
 	public Coordinates(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	
+	/**
+	 * Returns the current x coordinate
+	 * 
+	 * @return double x position
+	 */
 	public double getX() {
 		return this.x;
 	}
+	
+	
+	/**
+	 * Returns the current y coordinate
+	 * 
+	 * @return double y position
+	 */
 	public double getY() {
 		return this.y;
 	}
