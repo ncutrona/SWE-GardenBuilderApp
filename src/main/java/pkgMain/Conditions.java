b1c9e4dba97e1ba05d09c637be5078fed174f449
Khaleeb <caldavis@udel.edu>
2021-04-12 17:44:03 -0400

    add: Javadoc for Conditions class

--- src/main/java/pkgMain/Conditions.java
+++ src/main/java/pkgMain/Conditions.java
@@ -1,7 +1,24 @@
 package pkgMain;
 
+/**
+ * Conditions enumerators for sun, moisture, and soil condition.
+ * sunCondition: shade, partial, full.
+ * moistureCondition: dry, moist, wet.
+ * soilCondition: clay, loam, sand.
+ * 
+ * @author Aidan Chao
+ * @author Nicholas Cutrona
+ * @author Caleb Davis
+ * @author Joey Loporto
+ * @author Tommy Cheung
+ */
 public class Conditions {
 	
+	
+	/**
+	 * sunCondition enumerator.
+	 * SHADE, PARTIAL, FULL.
+	 */
 	public enum sunCondition {
 		
 		SHADE("shade"), 
@@ -11,15 +28,32 @@ public class Conditions {
 		
 		private String name;
 		
+		
+		/**
+		 * Sets name of sun condition from passed in string.
+		 * 
+		 * @param s string name of condition
+		 */
 		private sunCondition(String s) {
 			name = s;
 		}
 		
+		
+		/**
+		 * Returns the sun condition as a string
+		 * 
+		 * @return name of condition as string
+		 */
 		public String getName() {
 			return name;
 		}
 	}
 	
+	
+	/**
+	 * moistureCondition enumerator.
+	 * DRY, MOIST, WET.
+	 */
 	public enum moistureCondition {
 		
 		DRY("dry"), 
@@ -29,10 +63,22 @@ public class Conditions {
 		
 		private String name;
 		
+		
+		/**
+		 * Sets name of moisture condition from passed in string.
+		 * 
+		 * @param s string name of condition
+		 */
 		private moistureCondition(String s) {
 			name = s;
 		}
 		
+		
+		/**
+		 * Returns the sun condition as a string
+		 * 
+		 * @return name of condition as string
+		 */
 		public String getName() {
 			return name;
 		}
@@ -41,7 +87,10 @@ public class Conditions {
 	}
 	
 
-	
+	/**
+	 * soilCondition enumerator.
+	 * CLAY, LOAM, SAND.
+	 */
 	public enum soilCondition {
 		
 		CLAY("clay"), 
@@ -51,10 +100,22 @@ public class Conditions {
 		
 		private String name;
 		
+		
+		/**
+		 * Sets name of soil condition from passed in string.
+		 * 
+		 * @param s string name of condition
+		 */
 		private soilCondition(String s) {
 			name = s;
 		}
 		
+		
+		/**
+		 * Returns the sun condition as a string
+		 * 
+		 * @return name of condition as string
+		 */
 		public String getName() {
 			return name;
 		}
