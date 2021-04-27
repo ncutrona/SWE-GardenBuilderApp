3f030ae9af3e7d9efe8889818345fbf74d0340d5
Khaleeb <caldavis@udel.edu>
2021-04-27 19:34:09 -0400

    add: anchor javadoc

--- src/main/java/pkgMain/Anchor.java
+++ src/main/java/pkgMain/Anchor.java
@@ -8,9 +8,26 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.StrokeType;
 
+/**
+ * Anchor class, used to model the shape of the garden.
+ * 
+ * @author Aidan Chao
+ * @author Nicholas Cutrona
+ * @author Caleb Davis
+ * @author Joey Loporto
+ * @author Tommy Cheung
+ */
 public class Anchor extends Circle {
 	private final DoubleProperty x, y;
 
+	
+	/**
+	 * Creates new Anchor object.
+	 * Sets color, coords from params.
+	 * @param color Color color of anchor
+	 * @param x center X position for anchor
+	 * @param y center Y position for anchor
+	 */
 	public Anchor(Color color, DoubleProperty x, DoubleProperty y) {
 		super(x.get(), y.get(), 10);
 		setFill(color.deriveColor(1, 1, 1, 0.5));
