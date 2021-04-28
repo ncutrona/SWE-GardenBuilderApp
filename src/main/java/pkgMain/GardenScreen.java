a7c9ac5b1ff4ee7325400fc474b08d19a83ad0e4
Aidan Chao <achao@udel.edu>
2021-04-27 22:51:29 -0400

    ConditionsScreen, PentagonScreen, and GardenScreen now all have visually appealing buttons

--- src/main/java/pkgMain/GardenScreen.java
+++ src/main/java/pkgMain/GardenScreen.java
@@ -100,8 +100,23 @@ public class GardenScreen {
 	
 	public void createButton() {
 		inventory = new Button("See Full Inventory");
+		inventory.setStyle("-fx-padding: 8 15 15 15; -fx-background-insets: "
+				+ "0,0 0 5 0, 0 0 6 0, 0 0 7 0; -fx-background-radius: "
+				+ "8; -fx-background-color: linear-gradient(from 0% 93% to 0% 100%, #add8e6 0%, #add8e6 100%),"
+				+ "#add8e6,#add8e6,radial-gradient(center 50% 50%, radius 100%, #add8e6, #add8e6);"
+				+ "-fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );-fx-font-weight: bold; -fx-font-size: 1.1em;");
 		optionsButton = new Button("Options");
+		optionsButton.setStyle("-fx-padding: 8 15 15 15; -fx-background-insets: "
+				+ "0,0 0 5 0, 0 0 6 0, 0 0 7 0; -fx-background-radius: "
+				+ "8; -fx-background-color: linear-gradient(from 0% 93% to 0% 100%, #add8e6 0%, #add8e6 100%),"
+				+ "#add8e6,#add8e6,radial-gradient(center 50% 50%, radius 100%, #add8e6, #add8e6);"
+				+ "-fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );-fx-font-weight: bold; -fx-font-size: 1.1em;");
 		finish = new Button("Finish");
+		finish.setStyle("-fx-padding: 8 15 15 15; -fx-background-insets: "
+				+ "0,0 0 5 0, 0 0 6 0, 0 0 7 0; -fx-background-radius: "
+				+ "8; -fx-background-color: linear-gradient(from 0% 93% to 0% 100%, #add8e6 0%, #add8e6 100%),"
+				+ "#add8e6,#add8e6,radial-gradient(center 50% 50%, radius 100%, #add8e6, #add8e6);"
+				+ "-fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );-fx-font-weight: bold; -fx-font-size: 1.1em;");
 		
 	}
 	
