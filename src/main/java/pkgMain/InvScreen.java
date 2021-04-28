3330a764ec2f7b51c5ee8e08e1995156a1e0df5c
Aidan Chao <achao@udel.edu>
2021-04-27 23:35:12 -0400

    Updated button aesthetics for inventory screen and summary screen

--- src/main/java/pkgMain/InvScreen.java
+++ src/main/java/pkgMain/InvScreen.java
@@ -72,6 +72,11 @@ public class InvScreen {
 	 */
 	public void createButton() {
 		prevButtonInv = new Button("Go Back");
+		prevButtonInv.setStyle("-fx-padding: 8 15 15 15; -fx-background-insets: "
+				+ "0,0 0 5 0, 0 0 6 0, 0 0 7 0; -fx-background-radius: "
+				+ "8; -fx-background-color: linear-gradient(from 0% 93% to 0% 100%, #add8e6 0%, #add8e6 100%),"
+				+ "#add8e6,#add8e6,radial-gradient(center 50% 50%, radius 100%, #add8e6, #add8e6);"
+				+ "-fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );-fx-font-weight: bold; -fx-font-size: 1.1em;");
 	}
 	
 	/**
